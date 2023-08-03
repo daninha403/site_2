9724d6b155fa1d23adce9d2205355616f13e6d83
daninha403 <128608297+daninha403@users.noreply.github.com>
2023-08-03 19:05:36 -0300

    test9

--- receitas.js
+++ receitas.js
@@ -1,28 +1,26 @@
 function showRecipe(id) {
-    // Esconde os cards
     var cards = document.getElementsByClassName("card");
     for (var i = 0; i < cards.length; i++) {
         cards[i].style.display = "none";
     }
 
-    // Mostra a receita
     var recipe = document.getElementById(id);
     recipe.style.display = "block";
 }
 
 function hideRecipe(id) {
-     // Esconde a receita
+
      var recipe = document.getElementById(id);
      recipe.style.display = "none";
 
-     // Mostra os cards
+
      var cards = document.getElementsByClassName("card");
      for (var i = 0; i < cards.length; i++) {
          cards[i].style.display = "flex";
      }
 }
 
-// Slider de imagens
+
 var slides = document.getElementsByClassName("slide");
 var index = 0;
 
